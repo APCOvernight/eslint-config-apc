201c5ccfa4754ec132b015dc92176e6feefeac10
Ian Egner <ian.egner@apc-overnight.com>
2018-01-29 12:34:15 +0000

    Add new rules

--- test/validate-config.spec.js
+++ test/validate-config.spec.js
@@ -6,7 +6,10 @@ const CLIEngine = eslint.CLIEngine
 
 const cli = new CLIEngine({
   useEslintrc: false,
-  configFile: 'eslintrc.json'
+  configFile: 'eslintrc.json',
+  rules: {
+    'require-jsdoc': 0
+  }
 })
 
 describe('load config in eslint to validate all rule syntax is correct', () => {
@@ -20,5 +23,68 @@ describe('load config in eslint to validate all rule syntax is correct', () => {
     const code = 'const foo = 1;\nconst bar = function () {}\nbar(foo)\n'
 
     expect(cli.executeOnText(code).errorCount).to.equal(1)
+  })
+
+  it('Throw an error with var', () => {
+    const code = 'var foo = 1\nconst bar = function () {}\nbar(foo)\n'
+
+    expect(cli.executeOnText(code).errorCount).to.equal(1)
+  })
+
+  it('Throw a warning with console.log', () => {
+    const code = 'const foo = 1\nconst bar = function () {}\nconsole.log(bar(foo))\n'
+
+    expect(cli.executeOnText(code).warningCount).to.equal(1)
+  })
+
+  it('Allow console.info', () => {
+    const code = 'const foo = 1\nconst bar = function () {}\nconsole.info(bar(foo))\n'
+
+    expect(cli.executeOnText(code).warningCount).to.equal(0)
+  })
+
+  it('Throw an error with non camelcase variable', () => {
+    const code = 'const foo_foo = 1\nconst bar = function () {}\nbar(foo_foo)\n'
+
+    expect(cli.executeOnText(code).errorCount).to.equal(1)
+  })
+
+  it('Throw an error when arrow function can be used', () => {
+    const code = 'const foo = 1\nconst bar = function () {}\nbar(function () {\n  return foo\n})\n'
+
+    expect(cli.executeOnText(code).errorCount).to.equal(1)
+  })
+
+  it('Throw a warning with missing jsdoc', () => {
+    const code = 'const foo = 1\nconst bar = function () {}\nbar(foo)\n'
+
+    const cli = new CLIEngine({
+      useEslintrc: false,
+      configFile: 'eslintrc.json'
+    })
+    expect(cli.executeOnText(code).warningCount).to.equal(1)
+    expect(cli.executeOnText(code).errorCount).to.equal(0)
+  })
+
+  it('Error with invalid jsdoc', () => {
+    const code = 'const foo = 1\n/**\n * [description]\n * @return {[type]}\n [description]\n */\nconst bar = function (id) { return 2 }\nbar(foo)\n'
+
+    const cli = new CLIEngine({
+      useEslintrc: false,
+      configFile: 'eslintrc.json'
+    })
+    expect(cli.executeOnText(code).warningCount).to.equal(0)
+    expect(cli.executeOnText(code).errorCount).to.equal(1)
+  })
+
+  it('Allow with correct jsdoc', () => {
+    const code = 'const foo = 1\n/**\n * [description]\n * @param {String} id Given ID\n * @return {[type]}\n [description]\n */\nconst bar = function (id) { return 2 }\nbar(foo)\n'
+
+    const cli = new CLIEngine({
+      useEslintrc: false,
+      configFile: 'eslintrc.json'
+    })
+    expect(cli.executeOnText(code).warningCount).to.equal(0)
+    expect(cli.executeOnText(code).errorCount).to.equal(0)
   })
 })
